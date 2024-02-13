55b76a63695f4021c30de8ffdd2e6c8b8de9832c
JSalter90 <jamessalter90@gmail.com>
2024-02-13 16:45:02 +0000

    adding pseudo expriment function to source

--- papers/raikoke/0_Source.R
+++ papers/raikoke/0_Source.R
@@ -108,3 +108,49 @@ PredictBoth <- function(em1, em2, Design){
   return(list(overall = Pred1,
               met = Pred2))
 }
+
+
+# History matching experiment, treating each left-out run as observations in turn
+PseudoExperiment <- function(tData, val_inds, em_pred, obs_error){
+  
+  n <- length(val_inds)
+  val_data <- tData[val_inds,]
+  
+  overall_impl <- numeric(n) # implausibility of assumed obs in each experiment, for overall emulator
+  total_matches <- numeric(n) # how many METs are considered not implausible, in each experiment
+  overall_size <- pseudo_size <- cons_size <- numeric(n) # size of the different spaces
+  
+  for (i in 1:n){
+    pseudo_obs <- val_data$LogTotal[i]
+    
+    # Calculate overall implausibility
+    overall_impl[i] <- abs(em_pred$overall$Mean[val_inds[i]] - pseudo_obs) / sqrt(em_pred$overall$SD[val_inds[i]]^2 + obs_error)
+    
+    # Size of overall NROY space
+    impl_all <- (abs(em_pred$overall$Mean - pseudo_obs) / sqrt(em_pred$overall$SD^2 + obs_error))[val_inds]
+    overall_size[i] <- sum(impl_all < 3) / n 
+    
+    # Calculate implausibility for each MET
+    impl_MET <- matrix(0, n, 18)
+    for (j in 1:18){
+      impl_MET[,j] <- abs(em_pred$met[[j]]$Mean[val_inds] - pseudo_obs) / sqrt(em_pred$met[[j]]$SD[val_inds]^2 + obs_error)
+    }
+    
+    total_matches[i] <- sum(impl_MET[i,] < bound) # count how many METs the chosen 'obs' are in NROY for 
+    
+    # Size of pseudo NROY space
+    pseudo_size[i] <- sum(apply(impl_MET < 3, 1, sum) > 8) / n
+    
+    # Size of conservative NROY space
+    cons_size[i] <- sum(apply(impl_MET < 3, 1, sum) > 0) / n
+  }
+  
+  return(list(overall_impl,
+              overall_size,
+              total_matches,
+              pseudo_size,
+              cons_size))
+}
+
+
+
