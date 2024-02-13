d3a8fdfdcc993ca742691589d93c32a1e2e736ee
JSalter90 <jamessalter90@gmail.com>
2024-02-13 16:36:56 +0000

    typo

--- papers/raikoke/1_Emulation.R
+++ papers/raikoke/1_Emulation.R
@@ -4,7 +4,7 @@ val_inds <- readRDS("papers/raikoke/data/val_inds.rds")
 
 # Load input/output for each time point
 # These objects contain the 1000 runs, where
-# - each input has been scald to [-1,1]
+# - each input has been scaled to [-1,1]
 # - noise is a uniformly, randomly sampled term on [-1,1] to protect against overfitting mean functions
 # - LogTotal is the log of the total ash for the given timepoint/region
 tDataT3 <- readRDS("papers/raikoke/data/tDataT3.rds")
