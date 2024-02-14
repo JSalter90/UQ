98fd44c232ebcda5475c13dffd83c445c7e2689f
JSalter90 <jamessalter90@gmail.com>
2024-02-14 16:39:32 +0000

    making function more general

--- code/mogp_save.R
+++ code/mogp_save.R
@@ -9,11 +9,12 @@ SaveMulti <- function(ems, filename){
 }
 
 # Loading a list of MOGP emulators
-LoadMulti <- function(filename){
-  ell <- length(list.files(paste0(filename, '/'))) / 2
+# directory is location, filename is common tag
+LoadMulti <- function(directory, filename){
+  ell <- length(list.files(paste0(directory, '/'))) / 2
   tmp_list <- NULL
   for (i in 1:ell){
-    tmp_list[[i]] <- load_ExUQmogp(filename = paste0(filename, '/', filename, '_', i))
+    tmp_list[[i]] <- load_ExUQmogp(filename = paste0(directory, '/', filename, '_', i))
   }
   return(tmp_list)
 }
