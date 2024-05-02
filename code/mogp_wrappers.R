c6978a45cd38d0552df00630d25078c8e0c0d527
JSalter90 <jamessalter90@gmail.com>
2024-05-02 18:03:42 +0100

    renaming, merging all MOGP functions

--- code/mogp_wrappers.R
+++ code/mogp_wrappers.R
@@ -1,3 +1,25 @@
+# Saving a list of MOGP emulators
+SaveMulti <- function(ems, filename){
+  ell <- length(ems)
+  dir.create(filename)
+  for (i in 1:ell){
+    tmp_em <- ems[[i]]
+    save_ExUQmogp(tmp_em, filename = paste0(filename, '/', filename, '_', i))
+  }
+}
+
+# Loading a list of MOGP emulators
+# directory is location, filename is common tag
+LoadMulti <- function(directory, filename){
+  ell <- length(list.files(paste0(directory, '/'))) / 2
+  tmp_list <- NULL
+  for (i in 1:ell){
+    tmp_list[[i]] <- load_ExUQmogp(filename = paste0(directory, '/', filename, '_', i))
+  }
+  return(tmp_list)
+}
+
+
 # Create basis (DataBasis object) and other preliminary calculations (inverting W)
 CreateBasis <- function(data, type = 'SVD', W = NULL, RemoveMean = TRUE){
   # Invert W, creating Winv and tagging with attributes to enable fast calculations later on
