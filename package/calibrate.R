e469851e715e5637199d6be174c5fb03ed170884
JSalter90 <4189561+JSalter90@users.noreply.github.com>
2026-08-05 16:20:47 +0100

    Adding some documentation Exlicitly calling from stats

--- package/calibrate.R
+++ package/calibrate.R
@@ -1,32 +1,31 @@
-#' Functions for calibration
-
-#' From HM
-#' 
-#' 
-#' 
-#' NEW: disc paper
-#' 
-#' 
-#' 
-#' From SECRET?
-
-
-
-# Can you actually provide projected versions?
-
-# Make sure projections match up
-
-# When create basis or project, should store variance of each basis vector so don't need to recalculate
-
-# Also should store correct R_W where possible
-
-
-#' Wrapper for prediction + history matching, for single wave
+#' History matching
 #'
+#' Performing history matching, in 1D, for multiple independent outputs, or for high-dimensional fields, given observations, error (co)variances and corresponding emulators.
+#'
+#' This is a general function that can handle history matching across a wide variety of cases. Based on what is provided (some combination of emulators, sets of inputs to calculate the implausibility at, observations, ...), it recognises whether we are in the situation of multiple independent outputs or whether a single implausibility is to be calculated.
 #' 
+#' The possible cases, and the expected behaviour:
+#' 
+#' * `Obs` is a scalar, and so `emulator` should contain only a single emulator, and the 1D implausibility is calculated via `Impl1D`
+#' * `Obs` is a vector of length k, and `emulator` is a list of k emulators. Each of the k outputs is treated independently, and the 1D implausibility is calculated via `Impl1D`
+#' * `Obs` is a vector of length l, and `emulator` is a list of q < l emulators. Assumes that the leading q coefficients from `DataBasis$tBasis` have been emulated, and calculates `ImplField` (efficiently via basis decomposition)
+#' 
+#' Other cases (e.g., emulating l outputs independently, but wishing to calculate the l-dimensional implausiblity rather than l independent 1D implausibilities) are not supported by this function.
 #'
-#'
-#'
+#' @param emulator Either a single emulator, or a list of emulators, likely an object output by `FitEmulators`. If `NULL`,, then `TrueOutput` must be provided instead. 
+#' @param design 
+#' @param DataBasis If the multivariate implausibility is being calculated, need to provide the basis that the coefficents relate to. Likely an object output by `MakeDataBasis`.
+#' @param Obs The observations, z.
+#' @param ObsVar 
+#' @param DiscVar 
+#' @param TrueOutput 
+#' @param PreviousWave 
+#' @param ... 
+#' 
+#' @returns \item{Impl}{The implausibility (possible multiple) for each input, the size of NROY space, a logical vector, ...}
+#' \item{Design}{The inputs that the implausibility was evaluated at, including a column indicating whether each input is in NROY.}
+#' 
+#' @export
 HistoryMatch <- function(emulator = NULL, design = NULL, DataBasis = NULL, Obs, ObsVar, DiscVar = 0, TrueOutput = NULL, PreviousWave = NULL, ...){
   
   # If emulator not provided, must have been given TrueOutput, check
@@ -145,7 +144,7 @@ HistoryMatch <- function(emulator = NULL, design = NULL, DataBasis = NULL, Obs, 
 }
 
 
-#' History matching
+#' High-dimensional implausibility
 #'
 #' Calculates the implausibility (for the \ell-dimensional field) for a sample of expectations and variances from basis emulators
 #'
@@ -158,7 +157,7 @@ HistoryMatch <- function(emulator = NULL, design = NULL, DataBasis = NULL, Obs, 
 #' @param obs_inds If the observation vector is a subset of the full output, selects required indices from full output that correspond to observations. Defaults to NULL (all outputs observed)
 #' @param weightinv if not NULL, the inverse of W = var_err + var_disc, used for projection
 #' #' 
-#' @return \item{impl}{Vector of implausibilities corresponding to the rows of Expectation and Variance}
+#' @returns \item{impl}{Vector of implausibilities corresponding to the rows of Expectation and Variance}
 #' \item{bound}{The chi-squared bound for an \ell-dimensional field}
 #' \item{nroy}{Proportion of parameter settings that are not ruled out, using bound}
 #' \item{inNROY}{Vector indicating whether a parameter setting is ruled out}
@@ -251,7 +250,7 @@ ImplField <- function(Predictions, DataBasis, Obs, ObsVar, DiscVar = 0, obs_inds
                                         function(i) ImplCoeff(Expectation[i,], Variance[i,], ObsProj, WProj, 0*WProj),
                                         mc.cores = n_cores))
   impl <- impl + rep(R_W, n) 
-  bound <- qchisq(0.995, length(obs_inds))
+  bound <- stats::qchisq(0.995, length(obs_inds))
   nroy <- sum(impl < bound)/n
   inNROY <- impl < bound
 
@@ -307,6 +306,17 @@ ImplCoeff <- function(Expectation, Variance, Obs, ObsVar, DiscVar){
 
 
 
+#' High-dimensional implausibility without emulation
+#'
+#' @param Fields 
+#' @param Obs 
+#' @param ObsVar 
+#' @param DiscVar 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 ImplFieldTrue <- function(Fields, Obs, ObsVar, DiscVar = 0){
 
   ell <- nrow(Fields)
@@ -343,6 +353,12 @@ ImplFieldTrue <- function(Fields, Obs, ObsVar, DiscVar = 0){
 #' Instead of calculating via coeff + recon_error, could reconstruct everything
 #' Should be identical
 #' For testing only
+#'
+#' @param Predictions 
+#' @param DataBasis 
+#' @param Obs 
+#' @param ObsVar 
+#' @param DiscVar 
 ImplFieldRecon <- function(Predictions, DataBasis, Obs, ObsVar, DiscVar = 0){
   
   Expectation <- Predictions$Expectation
@@ -398,7 +414,7 @@ ImplFieldRecon <- function(Predictions, DataBasis, Obs, ObsVar, DiscVar = 0){
 SetDiscrepancy <- function(Basis, q, obs, level = 0.95, weightinv = NULL){
   TruncatedError <- ReconError(obs, Basis[,1:q], weightinv = weightinv)
   ell <- dim(Basis)[1]
-  b <- qchisq(level, ell)
+  b <- stats::qchisq(level, ell)
   DiscMultiplier <- c(TruncatedError / b)
   return(DiscMultiplier)
 }
@@ -418,7 +434,7 @@ SetDiscrepancy <- function(Basis, q, obs, level = 0.95, weightinv = NULL){
 DiscardedBasisVariance <- function(DataBasis, q, weightinv = NULL){
   BasMinusQ <- DataBasis$tBasis[,-(1:q)]
   DeletedCoeffs <- Project(DataBasis$CentredField, BasMinusQ, weightinv)
-  EstVar <- apply(DeletedCoeffs, 2, var)
+  EstVar <- apply(DeletedCoeffs, 2, stats::var)
   DeletedBasisVar <- BasMinusQ %*% diag(EstVar) %*% t(BasMinusQ)
   return(DeletedBasisVar)
 }
@@ -427,24 +443,51 @@ DiscardedBasisVariance <- function(DataBasis, q, weightinv = NULL){
 
 
 
-
-
-
-
-
-
+#' Title
+#'
+#' @param x 
+#' @param k 
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 kth_max <- function(x,k) {
   sorted_values <- sort(x, decreasing = TRUE)
   sorted_values[k]  # returns the kth maximum
 }
 
+
+
+
 # How many pass kth constraint
+#' Title
+#'
+#' @param impl 
+#' @param bound 
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 pass_kth_metric <- function(impl, bound = 3){
   ns <- nrow(impl)
   apply(impl < bound, 2, sum) / ns
 }
 
+
+
+
 # How many constraints each run passes. RENAME? pass_by_run?
+#' Title
+#'
+#' @param impl 
+#' @param bound 
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 pass_metric <- function(impl, bound = 3){
   ns <- nrow(impl)
   summary(as.factor(apply(impl < bound, 1, sum, na.rm = TRUE))) / ns
@@ -452,12 +495,7 @@ pass_metric <- function(impl, bound = 3){
 
 
 
-
-
-# given predictions, calculates implausibiltiy
-# wrap up with emulator later
-
-#' History matching
+#' Implausibility
 #'
 #' 1D impl for 1 or more metrics
 #'
@@ -532,6 +570,22 @@ Impl <- function(Predictions, Obs, ObsVar, DiscVar = 0, kmax = 1, bound = 3){
 }
 
 
+
+
+
+
+#' Title
+#'
+#' @param expectation 
+#' @param variance 
+#' @param Obs 
+#' @param ObsVar 
+#' @param DiscVar 
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 Impl1D <- function(expectation, variance, Obs, ObsVar, DiscVar = 0){
   abs(expectation - Obs) / sqrt(variance + ObsVar + DiscVar)
 }
@@ -541,6 +595,16 @@ Impl1D <- function(expectation, variance, Obs, ObsVar, DiscVar = 0){
 
 
 
+#' Title
+#'
+#' @param CandidatePoints 
+#' @param n_new 
+#' @param reps 
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 NewDesign <- function(CandidatePoints, n_new, reps = 100){
   
   # If final column(s) of CandidatePoints are NROY indicator, remove
@@ -690,33 +754,34 @@ PlotNROY <- function(Design, k = NULL, NROY = NULL, Truth = NULL){
 
 
 
+#### NOT EDITED ####
 
-PlotEmulator <- function(impl, design, variance = FALSE){
-  thetapred <- unit_to_theta(impl[,c('X1','X2')])
-  impl$lambda <- thetapred$lambda
-  impl$C <- thetapred$C
-  
-  if (!(is.null(dim(design)))){
-    design <- unit_to_theta(design)
-  }
-  
-  if (variance){
-    plot <- ggplot(impl, aes(lambda, C, col = Var)) + 
-      geom_point() +
-      geom_point(data = data.frame(C = design$C, lambda = design$lambda), col = 'black', size = 5, shape = 15) +
-      scale_colour_viridis_c(option = 'H') +
-      theme_minimal(base_size = 14) +
-      theme(panel.grid.minor = element_blank(), strip.text = element_text(face = "bold"))
-  }
-  
-  else{
-    plot <- ggplot(impl, aes(lambda, C, col = Mean)) + 
-      geom_point() +
-      geom_point(data = data.frame(C = design$C, lambda = design$lambda), col = 'black', size = 5, shape = 15) +
-      scale_colour_viridis_c() +
-      theme_minimal(base_size = 14) +
-      theme(panel.grid.minor = element_blank(), strip.text = element_text(face = "bold"))
-  }
-  
-  return(plot)
-}
+# PlotEmulator <- function(impl, design, variance = FALSE){
+#   thetapred <- unit_to_theta(impl[,c('X1','X2')])
+#   impl$lambda <- thetapred$lambda
+#   impl$C <- thetapred$C
+#   
+#   if (!(is.null(dim(design)))){
+#     design <- unit_to_theta(design)
+#   }
+#   
+#   if (variance){
+#     plot <- ggplot(impl, aes(lambda, C, col = Var)) + 
+#       geom_point() +
+#       geom_point(data = data.frame(C = design$C, lambda = design$lambda), col = 'black', size = 5, shape = 15) +
+#       scale_colour_viridis_c(option = 'H') +
+#       theme_minimal(base_size = 14) +
+#       theme(panel.grid.minor = element_blank(), strip.text = element_text(face = "bold"))
+#   }
+#   
+#   else{
+#     plot <- ggplot(impl, aes(lambda, C, col = Mean)) + 
+#       geom_point() +
+#       geom_point(data = data.frame(C = design$C, lambda = design$lambda), col = 'black', size = 5, shape = 15) +
+#       scale_colour_viridis_c() +
+#       theme_minimal(base_size = 14) +
+#       theme(panel.grid.minor = element_blank(), strip.text = element_text(face = "bold"))
+#   }
+#   
+#   return(plot)
+# }
